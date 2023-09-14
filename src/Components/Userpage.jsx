c7f510650fafb214d962e88b35bb053ea4a791bc
Rittik Chawla <rittik.ch@hotmail.com>
2023-09-14 12:36:28 +0700

    YeahBitch!!

--- src/Components/Userpage.jsx
+++ src/Components/Userpage.jsx
@@ -8,14 +8,16 @@ const Userpage = () => {
   return (
     <>
       <Navbar />
-      <main className='w-6/12 mx-auto '>
-        <h1 className='text-6xl p-5'>Generation Thailand</h1>
-        <p className='text-5xl p-5'>Home User-Sector</p>
-        <ButtonSector />
-      </main>
-      <UserTable />
+      <div className='w-6/12 mx-auto '>
+        <main className='p-5'>
+          <h1 className='text-6xl p-5'>Generation Thailand</h1>
+          <p className='text-5xl p-5'>Home User-Sector</p>
+          <ButtonSector />
+        </main>
+        <UserTable />
+      </div>
 
-    </>  )
+    </>)
 }
 
 export default Userpage
